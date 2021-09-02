fe06da44288aee8ec06674c11ffc18657dbe3399
abasheyari595 <84859238+abasheyari595@users.noreply.github.com>
2021-09-02 11:00:32 +0200

    Add files via upload

--- userbot/plugins/ترجمه.py
+++ userbot/plugins/ترجمه.py
@@ -33,10 +33,10 @@ async def getTranslate(text, **kwargs):
         "header": "لترجمة النص إلى اللغة المطلوبة.",
         "note": "لاكواد اللغه راجع [هذا اللينك](https://bit.ly/2SRQ6WU)",
         "usage": [
-            "{tr}<ترجمه <كود اللغه> ; <النص",
-            "{tr}<ترجمه <كود اللغه",
+            "{tr}ترجمه <كود اللغه> ; <النص>",
+            "{tr}ترجمه <كود اللغه>",
         ],
-        "examples": "{tr}ترجمه  + ar ; venom is one of the popular bot",
+        "examples": "{tr}ترجمه  + ar ; cat is one of the popular bot",
     },
 )
 async def _(event):
@@ -63,3 +63,94 @@ async def _(event):
         await edit_or_reply(event, output_str)
     except Exception as exc:
         await edit_delete(event, f"**خـطأ:**\n`{str(exc)}`", time=5)
+
+
+@catub.cat_cmd(
+    pattern="ترجم(?: |$)([\s\S]*)",
+    command=("ترجم", plugin_category),
+    info={
+        "header": "لترجمة النص إلى اللغة المطلوبة.",
+        "note": "لهذا قم بعمل ضبط ترجم وكود اللغه.",
+        "usage": [
+            "{tr}ترجم",
+            "{tr}ترجم <النص>",
+        ],
+    },
+)
+async def translateme(trans):
+    "لترجمة النص إلى اللغة المطلوب."
+    textx = await trans.get_reply_message()
+    message = trans.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        return await edit_or_reply(
+            trans, "`إعطاء نص أو الرد على رسالة للترجمة!`"
+        )
+    TRT_LANG = gvarstatus("TRT_LANG") or "en"
+    try:
+        reply_text = await getTranslate(deEmojify(message), dest=TRT_LANG)
+    except ValueError:
+        return await edit_delete(trans, "`Invalid destination language.`", time=5)
+    source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
+    transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
+    reply_text = f"**من {source_lan.title()}({reply_text.src.lower()}) إلي {transl_lan.title()}({reply_text.dest.lower()}) :**\n`{reply_text.text}`"
+
+    await edit_or_reply(trans, reply_text)
+    if BOTLOG:
+        await trans.client.send_message(
+            BOTLOG_CHATID,
+            f"`Translated some {source_lan.title()} stuff to {transl_lan.title()} just now.`",
+        )
+
+
+@catub.cat_cmd(
+    pattern="ضبط (شات البوت|ترجم) ([\s\S]*)",
+    command=("ضبط", plugin_category),
+    info={
+        "header": "لضبط اللغه للمترجم وشات البوت.",
+        "description": "اضغط هنا [لاكواد اللغه](https://bit.ly/2SRQ6WU)",
+        "options": {
+            "ترجم": "لغة المترجم الاصليه",
+            "شات بوت": "اللغه الاصليه لشات البوت(شات البوت)",
+        },
+        "usage": "{tr}ضبط option <language codes>",
+        "examples": [
+            "{tr}ضبط ترجم ar",
+            "{tr}ضبط شات البوت ar",
+        ],
+    },
+)
+async def lang(value):
+    "لضبط اللغه للمترجم وشات البوت."
+    arg = value.pattern_match.group(2).lower()
+    input_str = value.pattern_match.group(1)
+    if arg not in LANGUAGES:
+        return await edit_or_reply(
+            value,
+            f"`كود اللغه غير صالح ❗️`\n`اكواد اللغه المتاحه للمترجم`:\n\n`{LANGUAGES}`",
+        )
+    LANG = LANGUAGES[arg]
+    if input_str == "ترجم":
+        addgvar("TRT_LANG", arg)
+        await edit_or_reply(
+            value, f"`⌔︙ تغيرت لغة المترجم إلى {LANG.title()}.`"
+        )
+    else:
+        addgvar("AI_LANG", arg)
+        await edit_or_reply(
+            value, f"`⌔︙ تم تغيير لغة شات البوت إلى {LANG.title()}.`"
+        )
+    LANG = LANGUAGES[arg]
+
+    if BOTLOG:
+        if input_str == "ترجم":
+            await value.client.send_message(
+                BOTLOG_CHATID, f"`⌔︙ تغيرت لغة المترجم إلى {LANG.title()}.`"
+            )
+        if input_str == "شات البوت":
+            await value.client.send_message(
+                BOTLOG_CHATID, f"`⌔︙ تم تغيير لغة شات البوت إلى {LANG.title()}.`"
+            )

16dbb6ddcb98d308a40ac7e3f9645ed87acead77
abasheyari595 <84859238+abasheyari595@users.noreply.github.com>
2021-09-07 01:50:25 +0200

    Update تنظيف الرسائل.py

--- userbot/plugins/تنظيف الرسائل.py
+++ userbot/plugins/تنظيف الرسائل.py
@@ -106,7 +106,7 @@ async def purgeme(event):
     message = event.text
     count = int(message[9:])
     i = 1
-    async for message in event.client.iter_messages(event.chat_id, from_user="me"):
+    async for message in event.client.iter_messages(event.chat_id, from_user="لي"):
         if i > count + 1:
             break
         i += 1
